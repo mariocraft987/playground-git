6c665fd90ba45a06032b7457a0646ced3650653a
mariocraft <154646419+Mariocraft987@users.noreply.github.com>
2024-02-23 11:35:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log('Playground - test')
 
+
 function checkForm() {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
@@ -8,5 +9,6 @@ function checkForm() {
 document.getElementById('page').innerHTML = '<h2>Welcome ' + username + '!</h2>';
     }else {
 document.getElementById('msg').innerHTML = '<br/>Pls fill out the form!!';
+        window.error("form not filled out")
     }
 }
